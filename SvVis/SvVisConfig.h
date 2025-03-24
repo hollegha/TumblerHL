764b9ff8d06cdd31962d3f0236e6e7f39d5106ef
hollegha <hl@bulme.at>
2025-03-24 19:36:41 +0100

    Add files via upload

---
+++ SvVis/SvVisConfig.h
@@ -0,0 +1,28 @@
+
+// Achtung!! Keine Kommentare innerhalb der Sections
+// Com Schnittstelle 115200
+Com:  COM5 115200
+
+// Kanal Namen
+ChannelNames:
+	filt
+	frequ
+	ang
+	pow
+	forew
+	
+F_SAMPLE: 300
+
+// Y-Scales
+Y1Scale: -200 200
+Y2Scale: -200 200
+BarScale: -2000 2000
+
+// Graphen ( Kurven )
+Graphs:
+	xxx   Y1
+	xxx   Y1
+	ang   Y2
+	pow   Y2
+	forew Y2
+
