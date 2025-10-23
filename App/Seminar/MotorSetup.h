7cbf9b500918ac6d6641d93221a868345439349a
hollegha <hl@bulme.at>
2025-10-23 08:40:24 +0200

    Add files via upload

--- App/Seminar/MotorSetup.h
+++ App/Seminar/MotorSetup.h
@@ -2,6 +2,9 @@
 const int PWA = 4, AI1 = 32, PWB = 33, BI1 = 5, ST_BY = 18;
 const int M2A = 19, M1A = 23, M2B = 36, M1B = 39;
 
+// const int PWA = 4, AI1 = 16, PWB = 18, BI1 = 23, ST_BY = 17;
+// const int M2A = 19, M1A = 27, M2B = 25, M1B = 26;
+
 Motor motL(PWA, AI1, -1); // -1==Tumbler Motor
 Motor motR(PWB, BI1, -1);
 GpIoOut stdby(ST_BY);
