34f9e87549e9be42e59e776818e4ba3367515e30
hollegha <hl@bulme.at>
2025-11-11 01:12:45 +0100

    Add files via upload

--- SvVis/SvVisConfig.h
+++ SvVis/SvVisConfig.h
@@ -1,28 +1,26 @@
 
 // Achtung!! Keine Kommentare innerhalb der Sections
 // Com Schnittstelle 115200
-Com:  COM5 115200
+Com:  COM4  500000
+
+// IP: 192.168.4.1
 
 // Kanal Namen
 ChannelNames:
-	filt
-	frequ
-	ang
-	pow
-	forew
+	stick
+	gyro
+	gas
 	
-F_SAMPLE: 300
+F_SAMPLE: 50
 
 // Y-Scales
-Y1Scale: -200 200
-Y2Scale: -200 200
+Y1Scale:  100 2000
+Y2Scale:  -1000 1000
 BarScale: -2000 2000
 
 // Graphen ( Kurven )
 Graphs:
-	xxx   Y1
-	xxx   Y1
-	ang   Y2
-	pow   Y2
-	forew Y2
+	stick Y1
+	gyro  Y1
+	gas   Y1
 
