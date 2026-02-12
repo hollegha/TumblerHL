f2804ec09fe4fd788b2d963c678d0d7fda21a2e8
hollegha <hl@bulme.at>
2026-02-12 07:37:45 +0100

    Add files via upload

---
+++ App/patz/SvVis_Gyro.h
@@ -0,0 +1,26 @@
+
+// Achtung!! Keine Kommentare innerhalb der Sections
+// Com Schnittstelle 115200
+// Com: COM4 500000
+
+IP: 192.168.4.1
+
+// Kanal Namen
+ChannelNames:
+	soll
+	angle
+	omeg
+	
+F_SAMPLE: 100
+
+// Y-Scales
+Y1Scale: -1500 1500
+Y2Scale: -10 10
+BarScale: -2000 2000
+
+// Graphen ( Kurven )
+Graphs:
+	soll  Y1
+	angle Y1
+	xxx   Y2
+end
