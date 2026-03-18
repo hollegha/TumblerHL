7e7892a2dfe125054918a23a75d457ee88e1fb04
hollegha <hl@bulme.at>
2026-03-18 16:54:11 +0100

    Add files via upload

---
+++ App/patz/SvVis_Dist.h
@@ -0,0 +1,31 @@
+
+// Achtung!! Keine Kommentare innerhalb der Sections
+// Com Schnittstelle 115200
+// Com:  COM4 500000
+IP: 192.168.4.1
+
+// Kanal Namen
+ChannelNames:
+	encL
+	encR
+	dist
+	distF
+	
+F_SAMPLE: 100
+
+// Y-Scales
+Y1Scale: 0 3000
+Y2Scale:  -500 2000
+BarScale: -2000 2000
+
+// Graphen ( Kurven )
+Graphs:
+	xxx Y1
+	xxx Y1
+	xxx Y1
+	distF Y1
+end
+
+
+
+
