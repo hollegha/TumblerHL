5f60a4862f7df0f1c479a77ff8ca5d0738ad7e8a
hollegha <hl@bulme.at>
2026-06-18 10:33:07 +0200

    Add files via upload

---
+++ App/RpmContr/Sv_RpmC.h
@@ -0,0 +1,29 @@
+
+// Achtung!! Keine Kommentare innerhalb der Sections
+// Com Schnittstelle 115200
+Com:  COM4  500000
+// IP: 192.168.4.1
+
+// Kanal Namen
+ChannelNames:
+	soll
+	spL
+	spR
+	
+F_SAMPLE: 300
+
+// Y-Scales
+Y1Scale: -1200 1200
+Y2Scale: -900 900
+BarScale: -2000 2000
+
+// Graphen ( Kurven )
+Graphs:
+	soll Y1
+	spL  Y1
+	spR  Y1
+
+
+
+
+
